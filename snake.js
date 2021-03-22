cbd9ce9aa3de5452c67414bc5519e572f0c56e04
Dukewendo <dommct@yahoo.co.uk>
2021-03-22 21:21:55 +0000

    update monday

--- snake.js
+++ snake.js
@@ -21,7 +21,7 @@ function Snake() {
         this.xspeed = x;
         this.yspeed = y;
     }
-
+// eating food
     this.update = function() {
         for (var i=0;i < this.tail.length -1; i++) {
             this.tail[i] = this.tail[i+1];
