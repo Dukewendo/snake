8a25000b28df18fa81c3aea60d867691a4df7289
Dukewendo <dommct@yahoo.co.uk>
2021-03-24 21:49:08 +0000

    Wed update

--- snake.js
+++ snake.js
@@ -21,14 +21,27 @@ function Snake() {
         this.xspeed = x;
         this.yspeed = y;
     }
-// eating food nom nom nom
+
+    this.death = function() {
+        for (var i = 0; i< this.tail.length; i++) {
+            var pos = this.tail[i];
+            var d = dist(this.x, this.y, pos.x, pos.y);
+            if (d <1 ) {
+                alert("You died!")
+                total = 0;
+                this.tail = [];
+            }
+        }
+    }
+
     this.update = function() {
+        if (this.total === this.tail.length) {
         for (var i=0;i < this.tail.length -1; i++) {
             this.tail[i] = this.tail[i+1];
+            }
         }
-
         this.tail[this.total-1] = createVector(this.x, this.y);
-
+        
         this.x = this.x + this.xspeed*scl;
         this.y = this.y + this.yspeed*scl;
 
@@ -37,10 +50,11 @@ function Snake() {
     }
 
     this.show = function() {
-        for (var i = 0; i< this.total; i++) {
+        fill(255)
+        for (var i = 0; i< this.tail.length; i++) {
             rect(this.tail[i].x, this.tail[i].y, scl, scl);
         }
-        fill(255)
+       
         rect(this.x, this.y, scl, scl);
     }
 }
